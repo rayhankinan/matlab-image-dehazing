6d541d84493d1a4a87a8238698c07caaf1bc75e9
rayhankinan <rayhankinan@gmail.com>
2023-12-19 01:29:01 +0700

    Initialize utils

---
+++ src/+utils/computeAtmLight.m
@@ -0,0 +1,54 @@
+function atmLight = computeAtmLight(A, numLevels, winSize)
+% Quad-tree decomposition of dark channel is used for estimation
+% of atmospheric light as given in reference paper [2]
+
+[dm, dn, ~] = size(A);
+Q = [1, 1, dm, dn];
+
+% Create dark channel from non-overlapping patches
+minFiltStrel = strel('square', winSize);
+SE = min(A, [], 3);
+darkChannel = imerode(SE, minFiltStrel);
+
+% Iterate over quad-tree levels
+for ii = 1:numLevels
+    % Quadrants indices matrix
+    quadrantIndex = ([
+        Q(1), Q(2), (Q(1)+Q(3))/2, (Q(2)+Q(4))/2;
+        Q(1),((Q(2)+Q(4))/2)+1, (Q(1)+Q(3))/2, Q(4);
+        ((Q(1)+Q(3))/2)+1, Q(2), Q(3), ((Q(2)+Q(4))/2);
+        ((Q(3)+Q(1))/2)+1, ((Q(4)+Q(2))/2)+1, Q(3), Q(4)
+        ]);
+    quadrantIndex = round(quadrantIndex);
+    
+    % Decomposition of dark channel into four quadrants
+    firstQuadrant = darkChannel(quadrantIndex(1,1):quadrantIndex(1,3), quadrantIndex(1,2):quadrantIndex(1,4));
+    secondQuadrant = darkChannel(quadrantIndex(2,1):quadrantIndex(2,3), quadrantIndex(2,2):quadrantIndex(2,4));
+    thirdQuadrant = darkChannel(quadrantIndex(3,1):quadrantIndex(3,3), quadrantIndex(3,2):quadrantIndex(3,4));
+    fourthQuadrant = darkChannel(quadrantIndex(4,1):quadrantIndex(4,3), quadrantIndex(4,2):quadrantIndex(4,4));
+    
+    % Computation of mean for each quadrant
+    mu(1) = mean(firstQuadrant(:));
+    mu(2) = mean(secondQuadrant(:));
+    mu(3) = mean(thirdQuadrant(:));
+    mu(4) = mean(fourthQuadrant(:));
+    
+    % Selecting maximum average intensity quadrant
+    [~, ind] = max(mu);
+    Q = quadrantIndex(ind, :);
+end
+
+% Selecting bright image pixels based on final decomposed quadrant
+img = A(Q(1):Q(3), Q(2):Q(4), :);
+[mm, nn, pp] = size(img);
+brightIm = ones(mm, nn, pp);
+
+% Minimum Euclidean distance based bright pixel estimation (= atmLight)
+equiDist = sqrt((abs(brightIm - img)) .^ 2);
+equiDistImage = sum(equiDist, 3);
+equiDistVector = equiDistImage(:);
+imageVector = reshape(img, mm * nn, pp);
+[~, index] = min(equiDistVector);
+atmLight = imageVector(index, :);
+
+end
---
+++ src/+utils/dehaze.m
@@ -0,0 +1,50 @@
+function [correctedA, thicknessOfHaze, atmLight] = dehaze(A, epsilon, omega, minimumTransmissionValue)
+% SimpleDCP
+% This function computes dark-channel prior, and refines it using guided
+% filter, only across channel elements are considered for dark channel
+% estimation
+
+% Check if input image is RGB
+isRGB = size(A, 3) == 3;
+
+% 1. Estimate atmospheric light
+atmLight = utils.computeAtmLight(A, 5, 30);
+
+if isRGB
+    atmLight = reshape(atmLight, [1 1 3]);
+end
+
+% 2. Estimate transmission t(x)
+normI = min(A, [] , 3);
+minI = 1 - normI;
+
+% 3. Use guided filtering to refine the transmission map
+rawTransmissionMap = utils.algImguidedFilter(minI, minI, [5 5], epsilon);
+transitionMap = min(1, max(0, rawTransmissionMap));
+
+% Thickness of haze in input image is second output of imreducehaze.
+% Thickness Map does not depends on amount value.
+thicknessOfHaze = 1 - transitionMap;
+
+% Omega value is set to no more than 1, to leave some of haze in restored image for
+% natural appearance of dehazed scene
+omegaTransitionMap = 1 - omega * (1 - transitionMap);
+
+% This lower bound preserves a small amount of haze in dense haze regions
+t0 = cast(minimumTransmissionValue, 'like', A);
+
+% Recover scene radiance
+rawRadianceMap = atmLight + (A - atmLight) ./ max(omegaTransitionMap, t0);
+radianceMap = min(1, max(0, rawRadianceMap));
+
+% Corrected image
+correctedA = radianceMap;
+
+% Reshape atmLight to 1 x 3 vector if input image is RGB
+if isRGB
+    atmLight = double(reshape(atmLight, [1 3]));
+else
+    atmLight = double(atmLight);
+end
+
+end
